14521c6131de527bf838c9ad24d580eb5f561720
Stéphane Py <spy@mac-spy.linkingup.org>
2011-12-22 11:25:27 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+if [ -d $HOME/.vim ]
+then
+      echo "\033[0;33mYou already have a .vim dir installed.\033[0m You'll need to remove .vim if you want to install"
+    exit
+fi
+
+if [ -f $HOME/.vimrc ]
+then
+      echo "\033[0;33mYou already have a .vimrc installed.\033[0m You'll need to remove .vimrc if you want to install"
+    exit
+fi
+
+if [ -d $HOME/vim-config ]
+then
+      echo "\033[0;33mYou already have a vim-config dir installed.\033[0m You'll need to remove vim-config if you want to install"
+    exit
+fi
+
+
+echo "\033[0;34mCloning Vim config\033[0m"
+
+/usr/bin/env git clone https://github.com/BLEUROY-HIGHCO/vim-config.git $HOME/vim-config
+
+echo "\033[0;34mUpdate submodules\033[0m"
+
+cd $HOME/vim-config
+git submodule init
+git submodule update
+
+ln -s $HOME/vim-config/.vim $HOME/.vim
+ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+
+
+echo "\033[0;32m"'.__    .__       .__                         .__          '"\033[0m"
+echo "\033[0;32m"'|  |__ |__| ____ |  |__   ____  ____   ___  _|__| _____   '"\033[0m"
+echo "\033[0;32m"'|  |  \|  |/ ___\|  |  \_/ ___\/  _ \  \  \/ /  |/     \  '"\033[0m"
+echo "\033[0;32m"'|   Y  \  / /_/  >   Y  \  \__(  <_> )  \   /|  |  Y Y  \ '"\033[0m"
+echo "\033[0;32m"'|___|  /__\___  /|___|  /\___  >____/    \_/ |__|__|_|  / '"\033[0m"
+echo "\033[0;32m"'     \/  /_____/      \/     \/                       \/  '"\033[0m"
+
+echo "\n\n \033[0;32m....is now installed.\033[0m"
