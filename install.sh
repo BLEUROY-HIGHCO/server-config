e83d0b020f79d9a9d06e73c997476161cb48d16b
Stéphane Py <spy@mac-spy.linkingup.org>
2011-12-22 12:30:53 +0100

    Add installation system for bash

--- install.sh
+++ install.sh
@@ -1,41 +1,59 @@
 
 if [ -d $HOME/.vim ] || [ -h $HOME/.vim ]
 then
-      echo "\033[0;33mYou already have a .vim dir installed.\033[0m You'll need to remove .vim if you want to install"
-    exit
+    echo '.vim file already exists, delete it ? (y/n)' && read reponse
+    if [ "$reponse" == "y" ]
+    then
+        rm $HOME/.vim
+    else
+        exit
+    fi
 fi
 
 if [ -f $HOME/.vimrc ] || [ -h $HOME/.vimrc ]
 then
-      echo "\033[0;33mYou already have a .vimrc installed.\033[0m You'll need to remove .vimrc if you want to install"
-    exit
+    echo '.vimrc file already exists, delete it ? (y/n)' && read reponse
+    if [ "$reponse" == "y" ]
+    then
+        rm $HOME/.vimrc
+    else
+        exit
+    fi
 fi
 
 if [ -f $HOME/.bashrc ] || [ -h $HOME/.bashrc ]
 then
-      echo "\033[0;33mYou already have a .bashrc installed.\033[0m You'll need to remove .bashrc if you want to install"
-    exit
+    echo '.bashrc file already exists, delete it ? (y/n)' && read reponse
+    if [ "$reponse" == "y" ]
+    then
+        rm $HOME/.bashrc
+    fi
 fi
 
 if [ -d $HOME/server-config ]
 then
-      echo "\033[0;33mYou already have a server-config dir installed.\033[0m You'll need to remove server-config if you want to install"
-    exit
+    echo 'server-config file already exists, delete it ? (y/n)' && read reponse
+    if [ "$reponse" == "y" ]
+    then
+        rm $HOME/server-config
+    else
+        exit
+    fi
 fi
 
 
 echo "\033[0;34mCloning Vim config\033[0m"
 
-#/usr/bin/env git clone https://github.com/BLEUROY-HIGHCO/server-config.git $HOME/server-config
+/usr/bin/env git clone https://github.com/BLEUROY-HIGHCO/server-config.git $HOME/server-config
 
 echo "\033[0;34mUpdate submodules\033[0m"
 
-#cd $HOME/server-config
-#git submodule init
-#git submodule update
+/usr/bin/env cd $HOME/server-config
+/usr/bin/env git submodule init
+/usr/bin/env git submodule update
 
-#ln -s $HOME/server-config/.vim $HOME/.vim
-#ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+/usr/bin/env ln -s $HOME/server-config/.vim $HOME/.vim
+/usr/bin/env ln -s $HOME/.vim/.vimrc $HOME/.vimrc
 
 
 echo "\033[0;32m"'.__    .__       .__                         .__          '"\033[0m"
@@ -59,5 +77,11 @@ then
 fi
 fi
 
+echo "\033[0;32m"'.__    .__       .__                   ___.                 .__      '"\033[0m"
+echo "\033[0;32m"'|  |__ |__| ____ |  |__   ____  ____   \_ |__ _____    _____|  |__   '"\033[0m"
+echo "\033[0;32m"'|  |  \|  |/ ___\|  |  \_/ ___\/  _ \   | __ \\__  \  /  ___/  |  \  '"\033[0m"
+echo "\033[0;32m"'|   Y  \  / /_/  >   Y  \  \__(  <_> )  | \_\ \/ __ \_\___ \|   Y  \ '"\033[0m"
+echo "\033[0;32m"'|___|  /__\___  /|___|  /\___  >____/   |___  (____  /____  >___|  / '"\033[0m"
+echo "\033[0;32m"'     \/  /_____/      \/     \/             \/     \/     \/     \/  '"\033[0m"
 
 echo "\n\n \033[0;32m BASH ...is now installed.\033[0m"
